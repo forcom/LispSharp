913bfbe194b5fc358990b44c930ba90cbffa29fa
SeungYong Yoon <un6549@gmail.com>
2011-09-02 19:17:53 +0900

    Project Created

---
+++ Lisp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lisp {
+    class Program {
+        static void Main ( string [ ] args ) {
+            new lisp ( ).Start ( );
+        }
+    }
+}
---
+++ Lisp/lisp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lisp {
+    public class lisp {
+        public void Start ( ) {
+            while(true){
+            }
+        }
+    }
+}
